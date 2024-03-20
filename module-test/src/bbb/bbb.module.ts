e3931f21514a43797efcf0e961b5be456938a669
zhangxin <390755481@qq.com>
2024-03-20 17:43:47 +0800

    Module和Provider的循环依赖处理

---
+++ module-test/src/bbb/bbb.module.ts
@@ -0,0 +1,6 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { AaaModule } from 'src/aaa/aaa.module';
+@Module({
+  imports: [forwardRef(() => AaaModule)],
+})
+export class BbbModule {}
