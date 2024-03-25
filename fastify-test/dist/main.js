c6c5493a83e01024e68cefa8a9c2f5d369520b12
zhangxin <390755481@qq.com>
2024-03-25 16:39:36 +0800

    切换请求库fastify

---
+++ fastify-test/dist/main.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const platform_fastify_1 = require("@nestjs/platform-fastify");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_fastify_1.FastifyAdapter());
+    app.useStaticAssets({
+        root: `${__dirname}/../public`,
+        prefix: '/static',
+    });
+    await app.listen(3000);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
---
+++ fastify-test/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useStaticAssets({
+    root: `${__dirname}/../public`,
+    prefix: '/static',
+  });
+  await app.listen(3000);
+}
+bootstrap();
