76e4dd3b1fe4dc8052eb940b8fcc41f906fb80d4
zhangxin <390755481@qq.com>
2024-05-06 17:43:38 +0800

    multer文件上传

---
+++ nest-multer-upload/dist/app.controller.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="multer" />
+import { AppService } from './app.service';
+export declare class AppController {
+    private readonly appService;
+    constructor(appService: AppService);
+    getHello(): string;
+    uploadFile(file: Express.Multer.File, body: any): void;
+    uploadFiles(files: Array<Express.Multer.File>, body: any): void;
+    uploadFileFields(files: {
+        aaa?: Express.Multer.File[];
+        bbb?: Express.Multer.File[];
+    }, body: any): void;
+    uploadAnyFiles(files: Array<Express.Multer.File>, body: any): void;
+}
